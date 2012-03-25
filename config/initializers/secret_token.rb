af70b7e3f4bb5f30e118f0794f47a2cd6935756a
drupalmk <leisikkeram@gmail.com>
2012-03-25 20:28:30 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobeet::Application.config.secret_token = '5e2ac177e98aa7d8aa65c845dd9e6cb92b3ff52a10005012eb02c911fac7111fb8ac80d9e7e736fc23c1ce87fc7572cd0093dbdd3a33c491e4a13961344195e8'
