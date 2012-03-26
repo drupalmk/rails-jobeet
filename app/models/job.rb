daba52bc4370b6c99baf5eebef7e7886f84e5e58
drupalmk <leisikkeram@gmail.com>
2012-03-26 13:05:11 +0200

    setup logo upload

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,12 @@
 class Job < ActiveRecord::Base
+  
   belongs_to :category
   
   JOB_TYPES = { fulltime: 'Full time', parttime: 'Part time', freelance: 'Freelance' }
+  
+  JOB_UPLOADS_DIR = Rails.root.join('public', 'upload', 'jobs')
+  
+  attr_accessor :logo_file
   
   after_initialize :default_values
 
@@ -9,6 +14,24 @@ class Job < ActiveRecord::Base
     self.where("category_id = ? AND expires_at > ?", id, DateTime.now)
         .order("expires_at desc")
         .limit(limit)
+  end
+  
+  def upload
+     unless self.logo_file.nil?
+       o =  [('a'..'z')].map{|i| i.to_a}.flatten
+       random_name  =  (0..12).map{ o[rand(o.length)] }.join
+       file_ext = File.extname(self.logo_file.tempfile)
+       new_logo_filename = random_name + file_ext
+       
+       FileUtils.copy_file(self.logo_file.tempfile, Rails.root.join(Job::JOB_UPLOADS_DIR, new_logo_filename))
+
+       File.delete(Rails.root.join(Job::JOB_UPLOADS_DIR, self.logo)) unless self.logo.nil?
+
+       self.logo = new_logo_filename
+       self.logo_file = nil
+
+       return new_logo_filename
+     end
   end
   
   def save
